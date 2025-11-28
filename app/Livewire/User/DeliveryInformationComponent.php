3a0cdc475f3d99f8ff134843b327c16f5ffcaa1a
idris-kabri <kabriidris53@gmail.com>
2025-11-28 11:15:59 +0530

    Changes

---
+++ app/Livewire/User/DeliveryInformationComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User;
+
+use Livewire\Component;
+
+class DeliveryInformationComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.delivery-information-component');
+    }
+}
