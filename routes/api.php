07cf0ea1cfccbcef0628c7268dec2d2a37eda523
idris-kabri <kabriidris53@gmail.com>
2025-12-01 14:14:44 +0530

    Api changes

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InteractionController;
 
 Route::get('{platform}/interactions', [InteractionController::class, 'store']);
+Route::post('{platform}/interactions', [InteractionController::class, 'store']);
