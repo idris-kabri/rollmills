684930a2ffed51b680c3aa85e6b74daa13b16f77
idris-kabri <kabriidris53@gmail.com>
2026-01-02 15:48:01 +0530

    changes

--- app/Livewire/User/ShopDetailComponent.php
+++ app/Livewire/User/ShopDetailComponent.php
@@ -63,7 +63,7 @@ class ShopDetailComponent extends Component
         $relatedProduct = ProductRelation::where('product_id', $this->mainProduct->id)->where('type', 'Related')->pluck('related_product_id')->toArray();
         $linkedProduct = ProductRelation::where('product_id', $this->mainProduct->id)->where('type', 'Linked')->pluck('related_product_id')->toArray();
 
-        $this->relatedProducts = Product::whereIn('id', $relatedProduct)->where('parent_id', null)->take(10)->get();
+        $this->relatedProducts = Product::whereIn('id', $relatedProduct)->where('parent_id', null)->take(10)->orderBy('id', 'asc')->get();
         $this->linkedProducts = Product::whereIn('id', $linkedProduct)->get();
 
         if (count($this->relatedProducts) <= 0) {
@@ -73,6 +73,7 @@ class ShopDetailComponent extends Component
                 $query->whereIn('category_id', $categoryIds);
             })
                 ->where('id', '!=', $id)
+                ->orderBy('id', 'asc')
                 ->take(10)
                 ->get();
         }
