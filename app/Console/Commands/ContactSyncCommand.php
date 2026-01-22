12723837e99216a23ba3a58d86765f7813143fea
idris-kabri <kabriidris53@gmail.com>
2026-01-23 00:58:01 +0530

    changes

--- app/Console/Commands/ContactSyncCommand.php
+++ app/Console/Commands/ContactSyncCommand.php
@@ -26,7 +26,7 @@ class ContactSyncCommand extends Command
      */
     public function handle()
     {
-        $users = User::where('role', '!=', 'admin')->where('wawi_contact_id', null)->get();
+        $users = User::where('role', '!=', 'admin')->where('wawi_contact_id', null)->orderBy('id', 'desc')->get();
         foreach ($users as $user) {
             wawiContact($user);
         }
