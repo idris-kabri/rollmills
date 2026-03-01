09abd568a2507ea38933b46ac88f26ef3af1052f
idris-kabri <kabriidris53@gmail.com>
2026-03-01 22:02:33 +0530

    AWB Missing Order

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin;
 
 use App\Models\Order;
+use App\Models\OrderAWB;
 use App\Models\Product;
 use App\Models\ProductReview;
 use App\Models\User;
@@ -16,21 +17,23 @@ class Dashboard extends Component
     {
         $orderCount = Order::count();
         $productCount = Product::count();
-        $customerCount = User::where("role", "User")->count();
+        $customerCount = User::where('role', 'User')->count();
         $reviewCount = ProductReview::count();
 
         $today = Carbon::today();
 
-        $current_date_orders = Order::whereDate('created_at', $today) 
-        ->where('status', 1)
-        ->get();
+        $current_date_orders = Order::whereDate('created_at', $today)->where('status', 1)->get();
 
-        $current_register_user = User::where('is_guest_user', 0)->where('is_fake', 0)->where('id', '!=', 1)
-        ->get();
+        $current_register_user = User::where('is_guest_user', 0)->where('is_fake', 0)->where('id', '!=', 1)->get();
 
-        $current_guest_user_register = User::where('is_guest_user', 1)->where('is_fake', 0)->where('id', '!=', 1)
-        ->get();
+        $current_guest_user_register = User::where('is_guest_user', 1)->where('is_fake', 0)->where('id', '!=', 1)->get();
 
-        return view('livewire.admin.dashboard', compact('orderCount', 'productCount', 'customerCount', 'reviewCount','current_date_orders','current_register_user','current_guest_user_register'))->layout('layouts.admin.app');
+        $order_ids = OrderAWB::distinct()->pluck('order_id')->toArray();
+
+        $not_awbs = Order::whereNotIn('id', $order_ids)
+            ->whereNotIn('status', [0, 10, 1])
+            ->get();
+
+        return view('livewire.admin.dashboard', compact('orderCount', 'productCount', 'customerCount', 'reviewCount', 'current_date_orders', 'current_register_user', 'current_guest_user_register', 'not_awbs'))->layout('layouts.admin.app');
     }
 }
