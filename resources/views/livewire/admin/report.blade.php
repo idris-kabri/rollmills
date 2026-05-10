6cfa98609a03ac3427ffd7921dd2a94032dd49b8
idris-web <idris.kabri@atritechnocrat.com>
2026-05-10 16:03:45 +0530

    changes

--- resources/views/livewire/admin/report.blade.php
+++ resources/views/livewire/admin/report.blade.php
@@ -210,6 +210,7 @@
                                 <th class="text-end text-warning">Undelivered</th>
                                 <th class="text-end text-danger">Returned</th>
                                 <th class="text-end">RTO %</th>
+                                <th class="text-end">Return Shipping charges</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -222,6 +223,7 @@
                                 <td class="text-end text-warning">{{ $undelivered_ithink_order_count }}</td>
                                 <td class="text-end text-danger">{{ $returned_ithink_order_count }}</td>
                                 <td class="text-end fw-bold">{{ $ithink_rto_rate }}%</td>
+                                <td class="text-end fw-bold">{{ number_format($ithink_return_sum, 2) }}</td>
                             </tr>
                             <tr>
                                 <td><span class="fw-medium">ShadowFax</span></td>
@@ -232,6 +234,7 @@
                                 <td class="text-end text-warning">{{ $undelivered_shadow_fax_order_count }}</td>
                                 <td class="text-end text-danger">{{ $returned_shadow_fax_order_count }}</td>
                                 <td class="text-end fw-bold">{{ $shadow_fax_rto_rate }}%</td>
+                                <td class="text-end fw-bold">{{ number_format($shadow_fax_return_sum, 2) }}</td>
                             </tr>
                             <tr>
                                 <td><span class="fw-medium">XpressBees</span></td>
@@ -242,6 +245,7 @@
                                 <td class="text-end text-warning">{{ $undelivered_xpress_bees_order_count }}</td>
                                 <td class="text-end text-danger">{{ $returned_xpress_bees_order_count }}</td>
                                 <td class="text-end fw-bold">{{ $xpress_bees_rto_rate }}%</td>
+                                <td class="text-end fw-bold">{{ number_format($xpress_bees_return_sum, 2) }}</td>
                             </tr>
                         </tbody>
                     </table>
